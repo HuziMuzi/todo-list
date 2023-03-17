305ab2ed5bea8d15de96aa5dc5a42b14f992950a
HuziMuzi <laif17860@mail.ru>
2023-03-17 12:38:05 +0300

    Updates

---
+++ precache-manifest.d9c4c0d765ade7aa51f3a22436e70525.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20b022c07eb424b3742130832d02a2e3",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "092976c33ed288201bec",
+    "url": "/todo-list/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "74b26f97c108942f9a97",
+    "url": "/todo-list/static/js/2.abf1ffd7.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todo-list/static/js/2.abf1ffd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092976c33ed288201bec",
+    "url": "/todo-list/static/js/main.02ebc7d0.chunk.js"
+  },
+  {
+    "revision": "f5ee74d33c5afdf3bdbc",
+    "url": "/todo-list/static/js/runtime-main.bd857350.js"
+  }
+]);
